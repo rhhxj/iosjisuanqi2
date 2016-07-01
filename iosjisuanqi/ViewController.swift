4218fe9de3f670b042dd9142dad2c0be2f96d6e8
rhhxj <20141105047M@admindeiMac-08.local>
2016-07-01 17:41:05 +0800

    iOS简单计算器
    利用xcode进行简单的计算器，能进行加、减、乘、除的功能新加入了百分号和根号等按键，第一次使用ios的系统，很多东西都不会，况且电脑还出了问题
    ，困扰了我很久，但是最后问
    题解决了，我一个人用两台，这边上传，那边写程序。可能有些麻烦

--- iosjisuanqi/ViewController.swift
+++ iosjisuanqi/ViewController.swift
@@ -9,17 +9,148 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lableResult: UILabel!
+    
+    var firstOperand: Double = 0.0
+    var secondOperand: Double = 0.0
+    var decimalPointFlag: Bool = false
+    var isSecond: Bool = false
+    var operatorFlag: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    @IBAction func buttonTap(sender: UIButton) {
+        
+        if lableResult.text == "0" || (isSecond && secondOperand == 0.0) {
+            
+            lableResult.text = ""
+        }
+      
+        lableResult.text = lableResult.text! + sender.titleLabel!.text!
+        
+        if isSecond {
+           
+            secondOperand = NSString(string: lableResult.text!).doubleValue
+        }else {
+          
+            firstOperand = NSString(string: lableResult.text!).doubleValue
+        }
+        print(firstOperand)
+    }
+    
+    @IBAction func decimalPointTap() {
+        
+        if !decimalPointFlag {
+            lableResult.text = lableResult.text! + "."
+            if isSecond {
+                secondOperand = (lableResult.text! as NSString).doubleValue
+            }else {
+                firstOperand = (lableResult.text! as NSString).doubleValue
+            }
+            
+            decimalPointFlag = !decimalPointFlag
+        }
+    }
+    
+    
+    @IBAction func operatorTap(sender: UIButton) {
+        if firstOperand != 0 {
+            isSecond = true
+            decimalPointFlag = false
+            switch sender.titleLabel!.text! {
+            case "+":
+                operatorFlag = "+"
+            case "-":
+                operatorFlag = "-"
+            case "*":
+                operatorFlag = "*"
+            case "÷":
+                operatorFlag = "/"
+            case "√":
+                operatorFlag = "√"
+            case "x^2":
+                operatorFlag = "x^2"
+            case "1/x":
+                operatorFlag = "1/x"
+            default:
+                operatorFlag = " "
+            }
+        }
+    }
+    
+    
+    @IBAction func resultTap(sender: UIButton) {
+        
+        if isSecond {
+            
+            if operatorFlag == "/" && secondOperand == 0 {
+                print("Error: 除数不能为0")
+                return
+            }
+            
+            var result: Double = 0.0
+            switch operatorFlag {
+            case "+":
+                result = firstOperand + secondOperand
+            case "-":
+                result = firstOperand - secondOperand
+            case  "*":
+                result = firstOperand * secondOperand
+            case  "/":
+                result = firstOperand / secondOperand
+            case  "√":
+                result = sqrt(firstOperand)
+            case  "x^2":
+                result = pow(firstOperand, 2)
+            case  "1/x":
+                if firstOperand == 0 {
+                    print("Error: 不能为0")
+                    return
+                }
+                result = 1.0 / firstOperand
+            default :
+                result = 0.0
+            }
+            
+            lableResult.text = result.description
+            print("第一操作: \(firstOperand)")
+            print("操作符: \(operatorFlag)")
+            print("第二操作: \(secondOperand)")
+            print("结果: \(result)")
+            
+        }
+        
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        
+  
+        lableResult.text = "0"
+        
+      
+        firstOperand = 0.0
+        
+       
+        secondOperand = 0.0
+        
+      
+        decimalPointFlag = false
+        
+        
+        isSecond = false
+        
+        
+        operatorFlag = ""
+        
+    }
+    
 }
 
